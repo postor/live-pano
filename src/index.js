a9e4a491a4e7bdd8bace49f58c20bd9177320f17
josh <postor@gmail.com>
2020-03-14 17:35:59 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import playwebrtc from './playwebrtc'
 playwebrtc('webrtc://tencentplay.labpano.com/live/001',
   document.getElementById('video'),
   document.getElementById('pano'))
+
+window.triggerVideoPlay = () => {
+  document.getElementById('video').play()
+  document.getElementById('play').style.display = "none"
+}
