4d3661868edcd22512ce23f4327acfde91b45584
josh <postor@gmail.com>
2020-03-16 15:03:32 +0800

    pull after click

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import playwebrtc from './playwebrtc'
 
-playwebrtc('webrtc://tencentplay.labpano.com/live/001',
-  document.getElementById('video'),
-  document.getElementById('pano'))
 
 window.triggerVideoPlay = () => {
+  playwebrtc('webrtc://tencentplay.labpano.com/live/001',
+    document.getElementById('video'),
+    document.getElementById('pano'))
+    
   document.getElementById('video').play()
   document.getElementById('play').style.display = "none"
 }
