4dea969c5a8389762c893fd801b3b0fd02edb233
josh <postor@gmail.com>
2020-03-16 14:57:30 +0800

    remove muted

--- src/playwebrtc.js
+++ src/playwebrtc.js
@@ -3,7 +3,11 @@ import { VideoPanorama, Viewer } from 'panolens'
 import pullStream from './tx'
 
 export default async (webrtcUrl, videoElement, container) => {
-  const panorama = new VideoPanorama('', { autoplay: true, videoElement })
+  const panorama = new VideoPanorama('', {
+    autoplay: true,
+    videoElement,
+    muted: false
+  })
   panorama.setVideoCurrentTime = () => { }
   const viewer = new Viewer({ container, controlBar: false });
   viewer.add(panorama);
